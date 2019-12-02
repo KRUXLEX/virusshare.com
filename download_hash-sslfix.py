00c6005f9671d768daa91be7f28a9b3b0b7514e3
Axel <axel.gocan@grupawp.pl>
2019-12-02 16:00:40 +0100

    new file:   download_hash-sslfix.py 	modified:   download_hash.py 	new file:   download_hash.sh 	new file:   download_hash.sh.log

--- download_hash-sslfix.py
+++ download_hash-sslfix.py
@@ -12,7 +12,24 @@ import requests
 import os
 import sys
 
+from requests import adapters
+import ssl
+from urllib3 import poolmanager
+
 URL = 'https://virusshare.com/hashes/VirusShare_00'
+
+class TLSAdapter(adapters.HTTPAdapter):
+
+    def init_poolmanager(self, connections, maxsize, block=False):
+        """Create and initialize the urllib3 PoolManager."""
+        ctx = ssl.create_default_context()
+        ctx.set_ciphers('DEFAULT@SECLEVEL=1')
+        self.poolmanager = poolmanager.PoolManager(
+                num_pools=connections,
+                maxsize=maxsize,
+                block=block,
+                ssl_version=ssl.PROTOCOL_TLS,
+                ssl_context=ctx)
 
 
 def run(path, quiet=False):
@@ -24,6 +41,7 @@ def run(path, quiet=False):
     """
     if os.path.isdir(path):
         session = requests.Session()
+        session.mount('https://', TLSAdapter())
         session.headers = {'User-agent': 'Mozilla/5.0 Chrome/57.0.2987.110'}
         max_num = max(list(map(int, re.sub(r'[\<\>]',
                                            '',
